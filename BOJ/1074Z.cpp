d242d46f51211ab23a0ff2e3e29f27939198b585
JeongGoEun <wjdrhdms002@naver.com>
2020-02-11 16:26:59 +0900

    Solve BOJ no.1074, 1891

---
+++ BOJ/1074Z.cpp
@@ -0,0 +1,50 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+int N, R, C, r, c, ans, cnt;
+bool flag = false;
+void zFunc(int n, int y, int x) {
+	if (y == r && x == c) {
+		//여기서 바로 출력해주면 그 이후 값은 나타나지 않음
+		cout << ans;
+		return;
+	}
+
+
+	if (!(y <= r && r < y + n && x <= c && c < x + n)) {
+		ans += n * n;
+	}
+	else {
+		int mid = (n / 2);
+		//cout << y << ", " << x << ", " << n << endl;
+
+		//1, 2, 3, 4분면 다 검사
+		zFunc(mid, y, x);
+		zFunc(mid, y, x + mid);
+		zFunc(mid, y + mid, x);
+		zFunc(mid, y + mid, x + mid);
+	}
+}
+
+int main() {
+	cin >> N >> r >> c;
+	R = C = (int)pow(2, N);
+	zFunc(R, 0, 0);
+	return 0;
+}
+/*
+생각1
+- 반으로 줄여가면서 재귀로 풀었더니 시간초과
+- 사분면 순회 시 이 코드로 하면 임계값 설정이 어려워진다
+		-어차피 네번씩 돌꺼니까 너무 어렵게 생각X
+	for (int i = y; i < n; i+=mid) {
+		for (int j = x; j < n; j+=mid) {
+			중간 값 이후부터 시작
+			cout << i << ", " << j << ", " << mid << endl;
+			zFunc(mid, i, j);
+		}
+	}
+
+풀이
+- 범위를 계산해서 그 범위 안에 r,c가 없으면 그냥 전체 값을 더해주는 식으로
+*/
---
+++ BOJ/1891.cpp
@@ -0,0 +1,78 @@
+﻿#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+int N;
+long long x, y;
+string str;
+struct Node {
+	long long y, x;
+};
+Node s, e;
+
+void findStr(long long n, long long y, long long x, string curStr) {
+	if (curStr == str) {
+		//cout << y << ", " << x << endl;
+		s.y = y;
+		s.x = x;
+		return;
+	}
+
+	//1,2,3,4탐색
+	long long mid = n / 2;
+	int idx = curStr.size();
+
+	if (str[idx] == '1') {
+		findStr(mid, y, x + mid, curStr + "1");
+	}
+	else if (str[idx] == '2') {
+		findStr(mid, y, x, curStr + "2");
+	}
+	else if (str[idx] == '3') {
+		findStr(mid, y + mid, x, curStr + "3");
+	}
+	else {
+		findStr(mid, y + mid, x + mid, curStr + "4");
+	}
+}
+
+void findDst(long long n, long long y, long long x, string curStr) {
+	//cout << curStr << endl;
+	if (y == e.y && x == e.x) {
+		cout << curStr;
+		return;
+	}
+
+	// 범위에 없다면 볼 필요 없음
+	if (!(y <= e.y && e.y < y + n && x <= e.x && e.x < x + n)) {
+		return;
+	}
+	else {
+		//1,2,3,4탐색
+		long long mid = n / 2;
+		findDst(mid, y, x + mid, curStr + "1");
+		findDst(mid, y, x, curStr + "2");
+		findDst(mid, y + mid, x, curStr + "3");
+		findDst(mid, y + mid, x + mid, curStr + "4");
+	}
+}
+
+int main() {
+	cin >> N >> str >> x >> y;
+	long long n = (int)pow(2, N);
+
+	// 해당 좌표 찾기
+	findStr(n, 0, 0, "");
+
+	// 목적지 좌표 계산
+	e.y = s.y + ((-1)*y);
+	e.x = s.x + x;
+	//cout << e.y << ", " << e.x << endl;
+	if (e.y < 0 || e.x < 0 || e.y >= n || e.x >= n) {
+		cout << -1;
+	}
+	else {
+		findDst(n, 0, 0, "");
+	}
+	return 0;
+}
