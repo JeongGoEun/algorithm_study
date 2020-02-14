a6a07281b5a1ed9b823f60a46c1e13aa712e0e9d
JeongGoEun <wjdrhdms002@naver.com>
2020-02-14 15:11:58 +0900

    Solve BOJ no.1202, 1285

---
+++ BOJ/1202.cpp
@@ -0,0 +1,61 @@
+﻿#include<iostream>
+#include<set>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+struct Node {
+	int m, v;
+};
+multiset<int> s;
+vector<Node> v;
+int N, K;
+
+bool comp(Node n1, Node n2) {
+	return n1.v > n2.v;
+}
+int main() {
+	ios::sync_with_stdio(0);	cin.tie(0);
+	cin >> N >> K;
+	while (N--) {
+		Node n;
+		cin >> n.m >> n.v;
+		v.push_back(n);
+	}
+	sort(v.begin(), v.end(), comp);
+
+	while (K--) {
+		int w = 0;
+		cin >> w;
+		s.insert(w);
+	}
+
+	multiset<int>::iterator it;
+	long long sum = 0;
+
+	// 벡터 순회 && 가방이 비어있지 않을 때 까지
+	for (int i = 0; i < v.size() && !s.empty(); i++) {
+		Node cur = v[i];
+		bool flag = true;	//더 큰 애를 찾음
+		it = s.lower_bound(cur.m);
+		
+		// ! 이런 예외사항 꼭 처리 - 처음에 런타임에러 발생
+		if (it != s.end()) {
+			// 해당 무게를 찾았으면
+			sum += cur.v;
+			s.erase(it);
+		}
+	}
+	cout << sum;
+	return 0;
+}
+
+/*
+생각1
+- 무게를 내림차순, 가방 무게를 오름차순으로 정렬하고 적절한 최대무게를 가진 가방을 고르면 된다.
+	모든 방법을 다 탐색 =>최악의 경우 N^2 -> 90,000,000,000 시간초과 => 더 효율적인 방법을 찾아야 함
+
+풀이
+STL을 사용하여 더 효율적으로 탐색 - multiset!
+multiset의 lower_bound를 쓰면 효율적(해당 무게보다 큰 무게가 처음 나온 부분이니까)
+*/
---
+++ BOJ/1285.cpp
@@ -0,0 +1,59 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+int N;
+char map[20][20];
+void printf() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << (char)map[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+int main() {
+	ios::sync_with_stdio(0);	cin.tie(0);
+	string s = "";
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> s;
+		for (int j = 0; j < s.size(); j++) {
+			map[i][j] = s[j];
+		}
+	}
+	int ans = 987654321;
+	// 각 행
+	cout << (1 << N) << endl;
+	for (int k = 0; k < (1<<N); k++) {	//k행은 뒤집기로 결정
+		int tsum = 0;
+		for (int j = 0; j < N; j++) {
+			int tcnt = 0;
+
+			for (int i = 0; i < N; i++) {
+				char cur = map[i][j];
+				// 뒤집기로 한 행이라면 - 뒤집기
+				if ((1<<i) & k) {
+					cur = cur == 'T' ? 'H' : 'T';
+				}
+				if (cur == 'T') tcnt++;
+			}
+			// 해당 열을 뒤집는게 이득인지
+			tsum += min(tcnt, N - tcnt);
+		}
+		ans = min(ans, tsum);
+	}
+	cout << ans;
+	return 0;
+}
+/*
+생각
+1 - (i,j)기준으로 가로, 세로 T의 갯수를 세고 T의 갯수가 더 많고 그 갯수가 H의 갯수보다 크다면 바꿔줌
+
+풀이
+가정 : 모든 행에 대하여 뒤집을지 말지 결정되어 있다면
+		-> 열 별로 뒤집을지 말지 결정
+
+line27
+- 그냥 for문 변수로 하면 안되고 비트마스킹으로 해야 풀림
+*/
