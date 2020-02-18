b04863a3b1a88e65571b213106929036fc4ed272
JeongGoEun <wjdrhdms002@naver.com>
2020-02-18 23:34:59 +0900

    Add Programmers prob

---
+++ Programmers/완주하지못한선수.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    return answer;
+}
